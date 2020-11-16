067c6d29840697683c304f6e9982835bd7eadb85
tcparchem <tcparchem@gmail.com>
2020-11-16 12:55:12 -0600

    Implemented EmployeeListPage to properly retrieve data from the DB and populate into HTML table accordingly

--- frontend/src/pages/EmployeeListPage.js
+++ frontend/src/pages/EmployeeListPage.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 
-import Hero from '../components/Hero'
-import Table from 'react-bootstrap/Table'
+import * as ReactBootStrap from "react-bootstrap";
 
 
 class EmployeeListPage extends React.Component {
@@ -12,17 +11,19 @@ class EmployeeListPage extends React.Component {
 
     componentDidMount() {
         axios.get('http://localhost:3000/getEmployees').then(res => {
-            const employees = res.data;
+            const employees = res.data[0];
             this.setState({data: employees})
         })
     }
 
     renderEmployee = (employee, index) => {
+        if(employee == null) return
+
         return (
-            <tr> key={index} 
-                <td>employee.ID</td>
-                <td>employee.firstName</td>
-                <td>employee.lastName</td>
+            <tr key={index}>  
+                <td>{employee.ID}</td>
+                <td>{employee.firstName}</td>
+                <td>{employee.lastName}</td>
             </tr>
         )
     }
@@ -41,6 +42,9 @@ class EmployeeListPage extends React.Component {
                             <th>Last Name</th>
                         </tr>
                     </thead>
+                    <tbody>
+                        {this.state.data.map(this.renderEmployee)}
+                    </tbody>
                 </ReactBootStrap.Table>
             </div>
         );
